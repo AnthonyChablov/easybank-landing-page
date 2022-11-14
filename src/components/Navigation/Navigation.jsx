1ea4d515917bb5972d8b49fc8660ee31997f6053
Anthony Chablov <achablov@myseneca.ca>
2022-11-14 13:59:06 -0500

    inital commit

---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,13 @@
+import logo from '../../../assets/images/logo.svg';
+const Navigation = () => {
+    return (
+        <nav className="nav">
+            <div className="nav__logo">
+                <img src={logo} alt="" />
+            </div>
+
+        </nav>
+    )
+}
+
+export default Navigation

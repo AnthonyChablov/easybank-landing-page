c2bc18e31a78afe9b305050a72e9164e1af3de37
Anthony Chablov <achablov@myseneca.ca>
2022-11-18 13:03:07 -0500

    fix nav

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,5 +1,7 @@
-
+import React, { useEffect, useState } from "react";
 const Navigation = () => {
+    const [click, setClick] = useState(false);
+    const handleClick = () => setClick(!click);
     return (
         <nav className="nav">
             <div className="container">
@@ -7,24 +9,28 @@ const Navigation = () => {
                     <div className="nav__logo">
                         <img src={require('../../assets/images/logo.svg').default} alt="logo" />
                     </div>
-                    <div className="nav__menu">
-                        <ul className="menu__items">
+                    
+                        <ul className={click ? "nav-menu active" : "nav-menu"}>
                             <li className='items__list'><a href="#">Home</a></li>
                             <li className='items__list'><a href="#">About</a></li>
                             <li className='items__list'><a href="#">Contact</a></li>
                             <li className='items__list'><a href="#">Blog</a></li>
                             <li className='items__list'><a href="#">Careers</a></li>
                         </ul>
-                    </div>
+                    
                     <div className="nav__button">
                         <div className="button__wrapper">
                             Request Invite
                         </div>
                     </div>
-                    <div className="nav__hamburger">
-                        <button className='hamburger__button'>
-                            <img src={require('../../assets/images/icon-hamburger.svg').default} alt="logo" />
-                        </button>
+                    <div className="nav__hamburger " onClick={handleClick}>
+                    <button className='hamburger__button'>
+                        {click 
+                            ? <img src={require('../../assets/images/icon-close.svg').default} alt="logo"/>    
+                            : <img src={require('../../assets/images/icon-hamburger.svg').default} alt="logo"/> 
+                        }
+                    </button>
+                        
                     </div>
                 </div>
             </div>

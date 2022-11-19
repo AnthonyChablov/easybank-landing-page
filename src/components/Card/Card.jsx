f4e8440fd93bffc1525c324ebc14d4d7a6b87b3f
Anthony Chablov <achablov@myseneca.ca>
2022-11-19 17:00:03 -0500

    make card footer responsive

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,25 +3,27 @@ const Card = ({imgName,author, header,text }) => {
   return (
     <div className='card'>
       <div className="card__wrapper ">
-        <div className="card__img">
-          <Image 
-            imgName={imgName} 
-            author={author} 
-            header={header} 
-            text={text}
-          />
-        </div>
-        <div className="card__content">
-          <div className="card__author">
-            <p>{`By ${author}`}</p>
+        <a href="#">
+          <div className="card__img">
+            <Image 
+              imgName={imgName} 
+              author={author} 
+              header={header} 
+              text={text}
+            />
           </div>
-          <div className="card__header">
-            <p>{`${header}`}</p>
+          <div className="card__content">
+            <div className="card__author">
+              <p>{`By ${author}`}</p>
+            </div>
+            <div className="card__header">
+              <p>{`${header}`}</p>
+            </div>
+            <div className="card__text">
+              <p>{`${text}`}</p>
+            </div>
           </div>
-          <div className="card__text">
-            <p>{`${text}`}</p>
-          </div>
-        </div>
+        </a>
       </div>
     </div>
   )
